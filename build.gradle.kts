ffb781581d1a4aaa18fbb4c6bb662f2ac92d1c9f
bobthemogger <jackson.a.beyers1@gmail.com>
2026-09-06 19:15:08 -0500

    fix: replace alias() with id() and use Kotlin DSL for clean task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import org.gradle.api.tasks.Delete
+
 plugins {
-    alias("com.android.application") version "8.1.0" apply false
-    alias("com.android.library") version "8.1.0" apply false
-    alias("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-task clean(type) {
-    delete buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }

06a0ab246c017911f8c211136bd30a3f36dad462
bobthemogger <jackson.a.beyers1@gmail.com>
2026-09-06 19:16:18 -0500

    chore: add pluginManagement and dependencyResolutionManagement repositories (gradlePluginPortal + google + mavenCentral)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,14 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(org.gradle.api.initialization.dsl.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
         google()
         mavenCentral()
     }
